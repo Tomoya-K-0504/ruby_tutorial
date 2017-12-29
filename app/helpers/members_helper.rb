57a0f421929a539616c1d8563092dfa5a6453d3c
Tomoya Koike <makeffort134@gmail.com>
2017-12-29 16:53:55 +0900

    Create show action and add gravatar image

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -1,2 +1,9 @@
 module MembersHelper
+	def gravatar_for(member, option = { size: 80})
+		gravatar_id = Digest::MD5::hexdigest(member.mailaddress.downcase)
+		size = option[:size]
+		gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+		image_tag(gravatar_url, alt: member.name, class: "img-circle")
+		
+	end
 end
